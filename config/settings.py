18a91cdf96cd5aea61bc8a8245f1f07342b4dc08
Tze Yi <tzeyi@pantas.com>
2024-06-18 15:33:23 +0800

    fixed allowed_hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@7(ebqy#dd&5uou0921_f#f$@-st=9j*tdp!qgw4%5cd9z0=$i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = '*'
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
