3f6434825b8509e6e64698329d7d4103ebf46da8
Tze Yi <tzeyi@pantas.com>
2024-06-19 16:03:48 +0800

    removed explorers

--- explorers/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ExplorersConfig(AppConfig):
-    name = 'explorers'
--- explorers/constants.py
+++
@@ -1,9 +0,0 @@
-TABLE_COLUMN_MAP = {
-    "1": "name",
-    "2": "source",
-    "3": "year_released",
-    "4": "region",
-    "5": "sector",
-    "6": "category",
-    "7": "unit_type",
-}
