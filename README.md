c90d5ca2a92a780330035170aa1577eefe6d6c5b
Tze Yi <tzeyi@pantas.com>
2024-06-24 15:30:13 +0800

    update requirements.txt

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # To access aws shell
-ssh -i "tzeyi-aws-sns-test.pem" ubuntu@ec2-13-251-59-139.ap-southeast-1.compute.amazonaws.com
+ssh -i "tzeyi-aws-sns-test.pem" ubuntu@ec2-13-212-112-41.ap-southeast-1.compute.amazonaws.com
 
 add ubuntu@ before the public dns
--- sns_test/views.py
+++ sns_test/views.py
@@ -3,36 +3,21 @@ import json
 import re
 import requests
 from django.http import HttpResponse
-from requests import HTTPError
 from django.views.decorators.csrf import csrf_exempt
 
 
 logger = logging.getLogger(__name__)
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
 
 @csrf_exempt
 def receive_bloomberg_sns_message(request):
+    print(f'request.header: \n {request.headers}')
+
     if request.method == 'POST':
-
-        """
-        To test sns verifier:
-            temp = json.loads(request.body)
-            temp['MessageId'] = '123'
-            request._body = json.dumps(temp)
-        """
-        
-        temp = json.loads(request.body)
-        temp['MessageId'] = '123'
-        request._body = json.dumps(temp)
-
         message_type = request.headers['X-Amz-Sns-Message-Type']
         response_json = json.loads(request.body)
+        print(f'response_json: \n {response_json}')
         verify_sns(body_data=response_json)
-
-        print(f"response_json: \n {response_json}")
 
         # Subscription confirmation
         if message_type == 'SubscriptionConfirmation':
@@ -40,23 +25,20 @@ def receive_bloomberg_sns_message(request):
 
             requests.get(subscribe_url)
 
-            return HttpResponse(f"Subscription confirmed. Subscription confirmation request.body: {request.body}") 
+            return HttpResponse(f"Subscription confirmed. Subscription confirmation request.body: \n{request.body}")
 
         # Notification messages
         elif message_type == "Notification":
-            # Note: This second load is needed to parse the nested dict. 
+            # Note: This second load is needed to parse the nested dict.
             #       First load parses the nested dict as a string (eg: '{}', instead of {''})
             response_json = json.loads(response_json['Message'])
 
             file_key = response_json['generated']['data']['key']
             file_key = format_file_key(file_key)
-            print(f"file_key: {file_key}")
 
-            # client = BDLClient()
-            # datas = client.get_json_response(file_key)
+            print(file_key)
 
             # TODO: call store_to_model here
-
             return HttpResponse(f"notification response_json: \n{response_json}")
 
     return HttpResponse("normal get request")
@@ -68,7 +50,7 @@ def verify_sns(body_data):
         Aws sns verification: https://docs.aws.amazon.com/sns/latest/dg/SendMessageToHttp.prepare.html
         Cryptography: https://cryptography.io/en/latest/hazmat/primitives/asymmetric/rsa/
     """
-        
+
     import base64
     from cryptography import x509
     from cryptography.hazmat.backends import default_backend
@@ -83,47 +65,51 @@ def verify_sns(body_data):
     parsed_url = urlparse(sign_url)
 
     if parsed_url.scheme != 'https' or not parsed_url.hostname.endswith('.amazonaws.com'):
-        logger.warning("Bloomberg sns: Invalid SigningCertUrl. Url should be from amazonaws.com and be in https")
-        print("Bloomberg sns: Invalid SigningCertUrl. Url should be from amazonaws.com and be in https")
-        raise ValueError(f'Invalid SigningCertURL: {sign_url}')
-    
+        logger.warning(
+            f"Bloomberg sns: Invalid SigningCertUrl: {sign_url}. url should be from amazonaws.com and be in https")
+        raise ValueError
+
     certificate = requests.get(sign_url).text.encode('utf-8')
 
     # Extract the public key from the certificate.
-    public_key = x509.load_pem_x509_certificate(certificate, default_backend()).public_key()
+    public_key = x509.load_pem_x509_certificate(
+        certificate, default_backend()).public_key()
 
     # Create the string to sign.
     if body_data['Type'] == 'Notification':
         fields = 'Message', 'MessageId', 'Timestamp', 'TopicArn', 'Type'
     else:
         fields = 'Message', 'MessageId', 'SubscribeURL', 'Timestamp', 'Token', 'TopicArn', 'Type'
+
     canonical_msg = ''.join(f'{f}\n{body_data[f]}\n' for f in fields).encode()
 
     # Decode the Signature value from Base64 format.
     decoded_signature = base64.b64decode(body_data['Signature'])
-    
+
     # Verify the signature with the public key and the canonical message
-    # Note: SignatureVersion defaults to 1, which uses the SHA1 as its hash algoirthm
     try:
         if body_data['SignatureVersion'] == '1':
             public_key.verify(
-                decoded_signature, 
-                canonical_msg, 
+                decoded_signature,
+                canonical_msg,
                 padding.PKCS1v15(),
                 hashes.SHA1()
             )
 
         elif body_data['SignatureVersion'] == '2':
             public_key.verify(
-                decoded_signature, 
-                canonical_msg, 
-                padding.PKCS1v15(), 
+                decoded_signature,
+                canonical_msg,
+                padding.PKCS1v15(),
                 hashes.SHA256()
-                )
+            )
 
     except InvalidSignature:
-        print("Bloomberg sns: Invalid signature received. Need to check the validity of sender and message")
-        raise HTTPError('Invalid signature. Please check validity of the message')
+        logger.warning(
+            "Bloomberg sns: Invalid signature received. Need to check the validity of sender and message")
+        raise ValueError
+
+    return True
 
 
 def format_file_key(file_name):
